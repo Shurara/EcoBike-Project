f5a08a00870d73e7c0aa9cf4b5b2d55827911ebb
Oleksandr Rodiuk <o.rodiuk@gmail.com>
2020-01-20 18:11:19 +0200

    20012020 finish betting

--- src/main/java/DataWriter.java
+++ src/main/java/DataWriter.java
@@ -17,12 +17,15 @@ public class DataWriter {
 
         if (input.startsWith("FOLDING BIKE")) {
             String[] foldingBikeProps = input.substring(13).trim().split(";");
+            System.out.println(foldingBikeProps.length);
         }
         if (input.startsWith("SPEEDELEC")) {
             String[] speedelecProps = input.substring(10).trim().split(";");
+            System.out.println(speedelecProps);
         }
         if (input.startsWith("E-BIKE")) {
             String[] eBikeProps = input.substring(7).trim().split(";");
+            System.out.println(eBikeProps)
 
         }
 
