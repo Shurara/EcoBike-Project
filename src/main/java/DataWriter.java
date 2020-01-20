fc847871c71e0b1b1bfab8a4ed8a3020e4ec2420
Oleksandr Rodiuk <o.rodiuk@gmail.com>
2020-01-20 17:58:20 +0200

    created methods to get data from file + parse list of Strings

---
+++ src/main/java/DataWriter.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DataWriter {
+    public static void main(String[] args) {
+        Path path = Paths.get("src/ecobike.txt");
+        getDataFromFile(path).stream().forEach(System.out::println);
+    }
+
+    public static void parseModels(String input) {
+
+        if (input.startsWith("FOLDING BIKE")) {
+            String[] foldingBikeProps = input.substring(13).trim().split(";");
+        }
+        if (input.startsWith("SPEEDELEC")) {
+            String[] speedelecProps = input.substring(10).trim().split(";");
+        }
+        if (input.startsWith("E-BIKE")) {
+            String[] eBikeProps = input.substring(7).trim().split(";");
+
+        }
+
+    }
+
+    public static List<String> getDataFromFile(Path path) {
+
+        List<String> stringList = new ArrayList<>();
+        try {
+            stringList = Files.lines(path).collect(Collectors.toCollection(ArrayList::new));
+        } catch (IOException e) {
+            System.out.println("Файл не найден, введите еще раз");
+
+        }
+        return stringList;
+    }
+
+
+}
