e5e4e2a7eec8f04cf596a2a46577accb84ea40a5
Oleksandr Rodiuk <o.rodiuk@gmail.com>
2020-01-22 23:54:33 +0200

    created map of search parametrs and solved yes or no method

--- src/main/java/data_processing/SearchFunction.java
+++ src/main/java/data_processing/SearchFunction.java
@@ -9,30 +9,32 @@ import java.util.Scanner;
 public class SearchFunction {
     public static Map<String, String> getSearchValues() {
         Map<String, String> searchParameters = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
-        //FOLDING BIKE
+        //BIKE
         String brand = "brand";
-        String wheelsSizeInch = "wheelsSizeInch";
-        String gearsNumber = "gearsNumber";
-        String bikeWeight = "bikeWeight";
-        String isFrontAndBackLight = "isFrontAndBackLight";
+        String bikeWeight = "weight of the bike (in grams)";
+        String isFrontAndBackLight = "availability of lights at front and back (TRUE/FALSE)";
         String color = "color";
         String price = "price";
 
+        //FOLDING BIKE
+        String wheelsSizeInch = "size of the wheels (in inch)";
+        String gearsNumber = "number of gears";
+
+        //ELECTROBIKE
+        String maxSpeed = "maximum speed (in km/h)";
+        String batteryCapacity = "battery capacity (in mAh)";
+
+
         searchParameters.put(brand, DataChecker.getSearchParameters(brand));
-
         searchParameters.put(wheelsSizeInch, DataChecker.getSearchParameters(wheelsSizeInch));
-
         searchParameters.put(gearsNumber, DataChecker.getSearchParameters(gearsNumber));
-
         searchParameters.put(bikeWeight, DataChecker.getSearchParameters(bikeWeight));
-
         searchParameters.put(isFrontAndBackLight, DataChecker.getSearchParameters(isFrontAndBackLight));
-
         searchParameters.put(color, DataChecker.getSearchParameters(color));
-
         searchParameters.put(price, DataChecker.getSearchParameters(price));
-
+        searchParameters.put(maxSpeed, DataChecker.getSearchParameters(maxSpeed));
+        searchParameters.put(batteryCapacity, DataChecker.getSearchParameters(batteryCapacity));
+        System.out.println(searchParameters);
         return searchParameters;
     }
 
--- src/main/java/ui/DataChecker.java
+++ src/main/java/ui/DataChecker.java
@@ -25,38 +25,35 @@ public class DataChecker {
         String input;
         do {
             System.out.println(mistakeMessage);
-            /*while (!sc.hasNext()) {
-                System.out.println("String is empty! Try again)");
-                sc.next(); // this is important!
-            }*/
             input = sc.nextLine();
         } while (breakPredicate.test(input));
         System.out.println("Thank you!");
         return input;
     }
 
-   /* public static String getSearchParameters() {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        System.out.println("Thank you! Yo");
-        return input.isEmpty() ? null : input;
-    }*/
 
     public static String getSearchParameters(String parameter) {
-        System.out.printf("Would you like add the %s for searching? Please enter yes or no. ", parameter);
-        String ansver = getStringValue(str -> !"yes".equals(str) || !"no".equals(str), "Don't rush! Only yes or no!");
-        System.out.println("Thank you!");
+        System.out.printf("Would you like add the %s to search parametrs? \n", parameter);
+        System.out.println("Please enter yes or no");
         String value = "";
-
-        switch (ansver.toLowerCase()) {
-            case "yes":
-                value = DataChecker.getStringValue(str -> str == null || str.isEmpty(), "String must be not empty");
-            case "no":
-                value = null;
+        String ansver = getStringValue(str -> "yes".equalsIgnoreCase(str) && "no".equalsIgnoreCase(str), "Don't rush! Only yes or no!");
+        switch (ansver) {
+            case "yes": {
+                System.out.printf("Please input %s parametr \n", parameter);
+                value = getStringValue(str -> str == null || str.trim().isEmpty(), "String must be not empty");
+                break;
+            }
+            case "no": {
+                System.out.printf("parametr %s skipped \n", parameter);
+                break;
+            }
         }
         return value;
     }
-
 }
+
+
+
+
 
 
